977b4db15716a0484b9dec5f88032cffa6f4a8b4
Eric C <erix2205@gmail.com>
2020-07-09 09:18:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,14 @@
 function basicTeenager(age) {
   if (13<=age<=19){
+    return "You are a teenager!"
+}}
+
+function teenager(age) {
+  if (13<=age<=19){
     console.log("You are a teenager!")
   } else {
     console.log("You are not a teenager")
   }}
-
-function teenager(age) {
-
 
 function ageChecker(age) {
   if (age<=12){
@@ -18,9 +20,19 @@ function ageChecker(age) {
   }}
 
 function ternaryTeenager(age) {
-  12<age<20 ? "You are a teenager":"You are not a teenager"
+  return 12<age<20 ? "You are a teenager":"You are not a teenager"
 }
 
 function switchAge(age) {
-
+  case age===13:
+    console.log("You are a teenager");
+    break;
+  case age===14:
+    console.log("You are a teenager");
+    break;
+  case 14<age<=19:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
 }
